d1007805ba9bc417b6113ed292900193f396604b
Adesunmisola Adebajo <adesunmisolaadebajo@Adesunmisolas-MacBook-Air.local>
2022-05-03 14:04:13 +0100

    few changes

--- js/count.js
+++ js/count.js
@@ -1,7 +1,7 @@
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
-        document.getElementById("count").innerHTML = xhttp.responseText;
+        document.getElementById("visits").innerHTML = xhttp.responseText;
     }
 };
 xhttp.open("GET", "https://567znx3zkc.execute-api.us-east-1.amazonaws.com/Prod/visits/", true);
